1e8d10eee0a63525d520705598bea7720b935c1d
Sharath Sunil <alphatrinity17@gmail.com>
2021-02-12 01:37:38 -0500

    A Typical hello World Program

---
+++ HelloWorld.sh
@@ -0,0 +1,9 @@
+echo "Enter Something ?"
+read SomeThing
+#the variable after getting the value from the user 
+echo "$SomeThing"
+#the variable after getting the unsetting 
+unset SomeThing
+echo "$SomeThing"
+
+
